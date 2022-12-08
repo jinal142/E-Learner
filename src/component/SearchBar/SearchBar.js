3e590efc96556ab7124f50213eb727f97c94b109
VedantPShah <vedantshah80@gmail.com>
2022-12-08 00:03:00 -0600

    search suggestion

--- src/component/SearchBar/SearchBar.js
+++ src/component/SearchBar/SearchBar.js
@@ -5,30 +5,45 @@ import { Home } from '../../containers/Home/Home';
 import {Container, Form} from 'react-bootstrap';
 import './SearchBar.css';
 import { useNavigate } from 'react-router-dom';
+import { Component } from "react";
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 const SearchBar = () =>{
     const [search, setSearch] = useState("");
     //const [display, setDisplay] = useState(false);
     const navigate = useNavigate()
     
-    const  handlSearch = () => {
+    const items = [
+        {
+          name: 'Uber'
+        },
+        {
+          name: 'Airbnb'
+        },
+        {
+          name: 'Walmart'
+        },
+        {
+          name: 'Doordash'
+        },
+        {
+          name: 'Venmo'
+        }
+      ]
 
-            navigate("/results",{ state : {search} },{replace:true})
-            
-        
+  /*   const  handlSearch = () => {
+              
     }
 
     const handleSearchChnage = (e) => {
         // console.log(e.target.value);
         setSearch(e.target.value);
         // console.log(e.key)
-        
-        
-    };
+    }; */
 
     const handleEnter = (e) =>{
     
-        if(e.key === "Enter"){
+        
             // console.log(e.target.value)
             console.log("in search");
             const searchTerm = search.toUpperCase();
@@ -40,35 +55,62 @@ const SearchBar = () =>{
                 alert("Please Search from one of the following topics: \n Walmart, Uber, Venmo, Doordash, Airbnb");
                 return
             } else {
-                handlSearch()
+                navigate("/results",{ state : {search} },{replace:true})
              }
-        }
+        
     }
+
+    const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        //console.log(string, results)
+        setSearch(string)
+      }
+    
+      const handleOnHover = (result) => {
+        // the item hovered
+        console.log(result)
+      }
+    
+      const handleOnSelect = (item) => {
+        // the item selected
+        //console.log(item)
+        setSearch(item.name)
+      }
+    
+      const handleOnFocus = () => {
+        console.log('Focused')
+      }
+    const formatResult = (item) => {
+        return (
+          <>
+            <span>{item.name}</span>
+          </>
+        )
+      }
+
    return(
     <div className='home' style={{marginTop:"40px", height:"50px"}}>
-            
 
-            <div className="SearchContainer" class="d-flex justify-content-center" >
-                <Form className='form-control form-control-lg form-control-borderless'>
-                        <Form.Control  style={{width:"900px"}} onChange={(e) => {handleSearchChnage(e)}} 
-                            onKeyDown={(e)=>handleEnter(e)} value={search} type="text" placeholder="Search here" />           
-                        
-                </Form>
-            </div>    
-
-            &nbsp;
-            &nbsp;
-
-            <div className="SearchContainer" class="d-flex justify-content-center">
+        <div style={{ width: 800 }}>
+          <ReactSearchAutocomplete
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            items={items}
+            autoFocus
+            formatResult={formatResult}
+          />
+        </div>
+        <div className="SearchContainer" class="d-flex justify-content-center">
                 
-                    <button class="btn btn-lg btn-primary" onClick={handlSearch} style={{boxShadow: "5px 5px 5px rgba(0,0,0,.4)"}}> 
+                    <button class="btn btn-lg btn-primary" onClick={handleEnter} style={{boxShadow: "5px 5px 5px rgba(0,0,0,.4)"}}> 
                         <FiSearch style={{height:"25px", width:"25px"}}/>
                     </button>
            
-            </div>    
-
-            
-        </div>
+            </div>
+    </div>
    )
 
 }
